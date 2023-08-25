89dbe2efd54d93ecd485b95ae936663c81c7534b
Ameer Sabri <ameer.sabri@hotmail.co.uk>
2023-08-25 16:07:25 +0100

    feat: make twitter button component more resuable

--- src/components/TwitterButton.tsx
+++ src/components/TwitterButton.tsx
@@ -3,17 +3,27 @@ import TwitterLogo from "../assets/Twitter.svg";
 
 interface Props {
   rightMargin?: boolean;
+  text?: string;
+  mb?: string;
+  fullWidth?: boolean;
 }
 
-const TwitterButton: React.FC<Props> = ({ rightMargin = false }) => {
+const TwitterButton: React.FC<Props> = ({
+  rightMargin = false,
+  text = "Sign in with Twitter",
+  mb = "mb-2",
+  fullWidth = false,
+}) => {
   return (
     <button
-      className={`bg-twitter text-white flex p-3 md:px-6 md:py-4 mb-2 w-full md:w-fit md:mb-0 items-center text-base justify-center rounded-md font-semibold ${
+      className={`bg-twitter text-white flex p-3 md:px-6 md:py-4 ${mb} w-full ${
+        !fullWidth && "md:w-fit"
+      } md:mb-0 items-center text-base justify-center rounded-md font-semibold ${
         rightMargin && "md:mr-8"
       }`}
     >
       <img src={TwitterLogo} className="mr-2 md:mr-4" />
-      <p className="md:text-xl">Sign in with Twitter</p>
+      <p className="md:text-xl whitespace-nowrap">{text}</p>
     </button>
   );
 };
