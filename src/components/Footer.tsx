07d24e56f11d1d2cdaf163d59e8d8ea03018eb9a
Ameer Sabri <ameer.sabri@hotmail.co.uk>
2023-08-25 15:34:34 +0100

    fix: fix footer margins

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import TwitterBlack from "../assets/Twitter (Black).svg";
 
 const Footer = () => {
   return (
-    <div className="flex items-center justify-between md:px-10 px-4 lg:px-20 xl:px-32 2xl:px-64 mt-8 mb-8 flex-wrap md:flex-nowrap">
+    <div className="flex items-center justify-between md:px-10 px-4 lg:px-20 xl:px-32 2xl:px-64 mt-4 mb-4 flex-wrap md:flex-nowrap">
       <p className="text-primary italic font-bold text-xl md:text-2xl mb-2 md:mb-0">
         Chirp.
       </p>
